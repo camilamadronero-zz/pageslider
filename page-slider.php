abd85d5f5b670afb74cb9c9c1903881f0302f7b9
Camila Madroñero <camila@localhost.localdomain>
2019-03-04 21:39:21 -0500

    Create function file with headers

---
+++ page-slider.php
@@ -0,0 +1,29 @@
+defined( 'ABSPATH' ) or die( 'Be kind and leave' );
+
+<?php
+/**
+ * Plugin Name: Page Slider
+ * Plugin URI:  https://example.com/pageslider
+ * Description: Simple Page Slider Feature
+ * Version:     1
+ * Author:      Camila Madroñero
+ * Author URI:  https://author.example.com/
+ * License:     GPL2
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: wporg
+ * Domain Path: /languages
+ */
+Page Slider is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+any later version.
+
+Page Slider is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Page Slider. If not, see {License URI}.
+*/
+
---
+++ readme.txt
@@ -0,0 +1,35 @@
+=== Page Slider ===
+Contributors: Camila Madroñero
+Tags: Tags
+Donate link: Donate Link
+Requires at least: Required WordPress version
+Tested up to: Tested up to
+Requires PHP: Required PHP version
+Stable tag: Stable tag
+License: License
+License URI: License URI
+
+Short Description
+
+== Description ==
+Long Description
+
+
+== Installation ==
+Installation
+
+
+== Frequently Asked Questions ==
+FAQ
+
+
+== Screenshots ==
+1. Screenshot #1
+2. Screenshot #2
+3. Screenshot #3
+
+== Changelog ==
+Change Log
+
+== Upgrade Notice ==
+Upgrade Notice
