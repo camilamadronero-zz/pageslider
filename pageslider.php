b26ea04ddbd2b3b58b148c08bcbba3130367ab18
Camila Madroñero <camila@localhost.localdomain>
2019-03-10 17:06:21 -0500

    Fix comments closing tag

--- pageslider.php
+++ pageslider.php
@@ -12,7 +12,7 @@ defined( 'ABSPATH' ) or die( 'Be kind and leave' );
  * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: wporg
  * Domain Path: /languages
- */
+
 PageSlider is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
 the Free Software Foundation, either version 2 of the License, or
